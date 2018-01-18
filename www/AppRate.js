23072e10b51a7fbd52d8246faea3a3a5271c4e9f
Eduardo Kraus <kraus@eduardokraus.com>
2018-01-18 02:49:33 -0200

    Change

--- www/AppRate.js
+++ www/AppRate.js
@@ -1,24 +1,3 @@
-/*
-  *
-  * Licensed to the Apache Software Foundation (ASF) under one
-  * or more contributor license agreements. See the NOTICE file
-  * distributed with this work for additional information
-  * regarding copyright ownership. The ASF licenses this file
-  * to you under the Apache License, Version 2.0 (the
-  * "License"); you may not use this file except in compliance
-  * with the License. You may obtain a copy of the License at
-  *
-  * http://www.apache.org/licenses/LICENSE-2.0
-  *
-  * Unless required by applicable law or agreed to in writing,
-  * software distributed under the License is distributed on an
-  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-  * KIND, either express or implied. See the License for the
-  * specific language governing permissions and limitations
-  * under the License.
-  *
-  */
-;
 var AppRate, exec;
 
 exec = require ( 'cordova/exec' );
@@ -243,7 +222,6 @@ AppRate = (function () {
     };
 
     AppRate.preferences = {
-        useLanguage                  : null,
         displayAppName               : '',
         simpleMode                   : false,
         promptAgainForEachNewVersion : true,
@@ -265,16 +243,7 @@ AppRate = (function () {
         if ( immediately == null ) {
             immediately = true;
         }
-        if ( this.preferences.useLanguage === null ) {
-            navigator.globalization.getPreferredLanguage ( (function ( _this ) {
-                return function ( language ) {
-                    _this.preferences.useLanguage = language.value;
-                    return showDialog ( immediately );
-                };
-            }) ( this ) );
-        } else {
-            showDialog ( immediately );
-        }
+        showDialog ( immediately );
         updateCounter ();
         return this;
     };
